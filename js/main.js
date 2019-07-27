f69ecbb663c1c7df41aa6c18314bfccaedc69bd9
xaca <and1res@hotmail.com>
2019-07-26 20:25:41 -0500

    Se configura la comunicacion con el end point

--- js/main.js
+++ js/main.js
@@ -4,13 +4,19 @@ window.onload = init;
 
 
 function init(){
-    
+
 }
 
 function realizarPeticion(){
-
+    //https://developer.mozilla.org/es/docs/Web/API/Fetch_API/Utilizando_Fetch
+    let url_end_point = "https://api.github.com/users/codeyourfuture/repos";
+    fetch('http://example.com/movies.json')
+    .then(function(response) {
+        return response.json();
+    })
+    .then(procesarPeticion);
 }
 
-function procesarPeticion(){
-
+function procesarPeticion(json){
+    console.log(json);
 }

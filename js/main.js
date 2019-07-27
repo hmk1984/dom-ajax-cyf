943af9ac407a4c71c5844052e872f8475d766eb9
xaca <and1res@hotmail.com>
2019-07-26 21:17:12 -0500

    Enlace al repositorio

--- js/main.js
+++ js/main.js
@@ -7,6 +7,10 @@ function inicio(){
     realizarPeticion();
 }
 
+function irARepositorio(event){
+    window.open(repositorio.options[repositorio.selectedIndex].value, "_blank");
+}
+
 function realizarPeticion(){
     //https://developer.mozilla.org/es/docs/Web/API/Fetch_API/Utilizando_Fetch
     let url_end_point = "https://api.github.com/users/codeyourfuture/repos";
@@ -19,15 +23,18 @@ function realizarPeticion(){
 
 function procesarPeticion(json){
     //console.log(json);
-    //Ejemplo de respuesta
+    //Ejemplo de respuesta;
     /* 0: Object { id: 75015169, node_id: "MDEwOlJlcG9zaXRvcnk3NTAxNTE2OQ==", name: "api-demo", … }
     1: Object { id: 82218620, node_id: "MDEwOlJlcG9zaXRvcnk4MjIxODYyMA==", name: "api-proxies", … }
     ​2: Object { id: 103732853, node_id: "MDEwOlJlcG9zaXRvcnkxMDM3MzI4NTM=", name: "api-workshop", … } */
-    let combo_de_prueba = "<select>";
+    let _html = ""
+    let combo_de_prueba = "<select id='repositorio' onchange='irARepositorio(this)'>";
+    let link = "";
     for(i in json)
     {
-        combo_de_prueba+= "<option>"+json[i].name+"</option>";
+        combo_de_prueba+= `<option  value="${json[i].html_url}">${json[i].name}</option>`;
     }
     combo_de_prueba += "</select>";//Ojo no es eficiente, pero de esta forma entendemos como funciona
+
     respuesta.innerHTML = combo_de_prueba;
 }

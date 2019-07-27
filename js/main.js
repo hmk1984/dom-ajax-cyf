88308ae39481b13a1567f209e891af50cb97c32b
xaca <and1res@hotmail.com>
2019-07-26 20:40:35 -0500

    ¡Ejemplo de combo listo! :muscle:

--- js/main.js
+++ js/main.js
@@ -18,5 +18,16 @@ function realizarPeticion(){
 }
 
 function procesarPeticion(json){
-    console.log(json);
+    //console.log(json);
+    //Ejemplo de respuesta
+    /* 0: Object { id: 75015169, node_id: "MDEwOlJlcG9zaXRvcnk3NTAxNTE2OQ==", name: "api-demo", … }
+    1: Object { id: 82218620, node_id: "MDEwOlJlcG9zaXRvcnk4MjIxODYyMA==", name: "api-proxies", … }
+    ​2: Object { id: 103732853, node_id: "MDEwOlJlcG9zaXRvcnkxMDM3MzI4NTM=", name: "api-workshop", … } */
+    let combo_de_prueba = "<select>";
+    for(i in json)
+    {
+        combo_de_prueba+= "<option>"+json[i].name+"</option>";
+    }
+    combo_de_prueba += "</select>";//Ojo no es eficiente, pero de esta forma entendemos como funciona
+    respuesta.innerHTML = combo_de_prueba;
 }
